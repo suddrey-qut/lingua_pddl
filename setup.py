3aeada05992fbd98f8ba289ae89f90250816c064
Gavin Suddrey <gavin.suddrey@cirrusrobotics.com.au>
2020-01-11 21:13:35 +1000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(packages=['lingua_pddl'], package_dir={'': 'src'})
+
+setup(**d)
